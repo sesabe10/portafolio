ea4a9ab3ea84cc85ba36b90113960ece81b1d6c0
Sebastián Sánchez Bedoya <sesabe10@gmail.com>
2025-02-09 16:12:57 -0500

    se agregaron validaciones al formulario

--- src/app/contac/contac/contac.component.ts
+++ src/app/contac/contac/contac.component.ts
@@ -4,18 +4,20 @@ import { EmailServiceService } from 'src/app/email-service.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DatosContacto } from 'src/app/datos-contacto';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contac',
   standalone: true,
-  imports: [NavComponent, ReactiveFormsModule, HttpClientModule],
+  imports: [NavComponent, ReactiveFormsModule, HttpClientModule, CommonModule],
   templateUrl: './contac.component.html',
   styleUrls: ['./contac.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContacComponent {
+export class ContacComponent { 
 
-  constructor(private emailService: EmailServiceService) {}
+  constructor(private emailService: EmailServiceService) {    
+  }  
 
   userForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
