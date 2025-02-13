b1be58cb3d7f53fc6e109409ee9d9700e441c558
Sebastián Sánchez Bedoya <sesabe10@gmail.com>
2025-02-12 22:36:50 -0500

    re arreglan rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { ContacComponent } from './contac/contac/contac.component';
 import { PortafolioComponent } from './portafolio/portafolio.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: PortafolioComponent, data:{animation: 'homePage'}},
-  {path: 'contac', component: ContacComponent, data: {animation: 'contacPage'}}
+  {path: 'contac', component: ContacComponent, data: {animation: 'contacPage'}},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
